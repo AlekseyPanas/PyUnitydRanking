c3aae41f7490b76dc7b7d7f524daa050a8fd5684
root <root@ip-172-31-26-173.us-west-2.compute.internal>
2020-02-18 21:37:31 +0000

    coll shiz

---
+++ script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(() => {
+//	console.log("background-size", "100% " + (($("#list-container").height() - 72) + "px"));
+	//$(".grad").css("background-size", "100% " + (($("#list-container").height() + 127) + "px"));
+	const desiredHeight =  "100% " + (($("#list-container").height() < $(window).height()) ? ($("#list-container").height() + 127) : $(window).height()) + "px";
+
+	console.log(desiredHeight);
+
+	$(".grad").css("background-size", desiredHeight);
+});
+
+
+
