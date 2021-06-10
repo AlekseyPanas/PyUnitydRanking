564caddb28941f1cdd6f2540dd4f51698ea8b21a
root <root@ip-172-31-26-173.us-west-2.compute.internal>
2021-06-10 17:34:00 +0000

    update rankings

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-$(document).ready(() => {
+$(window).on("load", () => {
 //	console.log("background-size", "100% " + (($("#list-container").height() - 72) + "px"));
 	//$(".grad").css("background-size", "100% " + (($("#list-container").height() + 127) + "px"));
 	const desiredHeight =  "100% " + (($("#list-container").height() < $(window).height()) ? ($("#list-container").height() + 127) : $(window).height()) + "px";
