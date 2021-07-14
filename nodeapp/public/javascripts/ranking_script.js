2335548e85aa53c848b7fc008fbb51a44ba507fb
AlekseyPanas <panasaleksey3@gmail.com>
2021-07-14 19:46:11 -0400

    Make rankings responsive

--- nodeapp/public/javascripts/ranking_script.js
+++ nodeapp/public/javascripts/ranking_script.js
@@ -1,13 +1,18 @@
 
 $(document).ready(() => {
-//	console.log("background-size", "100% " + (($("#list-container").height() - 72) + "px"));
-	//$(".grad").css("background-size", "100% " + (($("#list-container").height() + 127) + "px"));
+	// Resizes bg on resize of viewport
+	$(window).resize(resize_bg);
+	resize_bg();
+
+	
+});
+
+// Resizes gradient bg to fit fullscreen
+function resize_bg () {
 	const desiredHeight =  "100% " + (($("#list-container").height() < $(window).height()) ? ($("#list-container").height() + 127) : $(window).height()) + "px";
 
 	console.log(desiredHeight);
 
 	$(".grad").css("background-size", desiredHeight);
-});
-
-
+}
 
