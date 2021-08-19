b9c438bdfe2e7b231abd01e7884ab27cca227fbe
AlekseyPanas <panasaleksey3@gmail.com>
2021-08-19 16:15:00 -0400

    Add dashboard skeleton

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var session = require('express-session')
+var session = require('express-session');
+var db = require("./db/db");
 
 var indexRouter = require('./routes/index');
 
@@ -33,14 +34,16 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(async function(err, req, res, next) {
+  let account = await db.get_account_by_id(req.session.account_id);
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', {page: "error"});
+  res.render('error', {page: "error", account: account});
 });
 
 module.exports = app;
